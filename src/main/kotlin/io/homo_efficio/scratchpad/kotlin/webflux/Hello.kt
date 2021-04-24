66cca40e35417356cac67ab7794fc27dd5aeb7ad
boojongmin <jongmin.boo@way2bit.com>
2021-04-24 09:37:27 +0900

    fix test code - new hello message on suspend fun

--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/Hello.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/Hello.kt
@@ -2,10 +2,10 @@ package io.homo_efficio.scratchpad.kotlin.webflux
 
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import javax.validation.Valid
+import javax.validation.constraints.Size
 
 @RequestMapping("/hello")
 @RestController
@@ -18,16 +18,27 @@ class HelloController {
         return Mono.just(ResponseEntity.ok(name))
     }
 
-//    @PostMapping
-//    fun newHello(@Valid @RequestBody msg: HelloMessage):
-//            ResponseEntity<HelloMessage> {
-//
-//        return ResponseEntity.ok(msg)
-//    }
     @PostMapping
-    fun newHello(@Valid @RequestBody msg: HelloMsgJava):
-            ResponseEntity<HelloMsgJava> {
+    fun newHello(@Valid @RequestBody msg: HelloMessage):
+            ResponseEntity<HelloMessage> {
 
         return ResponseEntity.ok(msg)
     }
+
+//    @PostMapping
+//    fun newHello(@Valid @RequestBody msg: HelloMsgJava):
+//            ResponseEntity<HelloMsgJava> {
+//
+//        return ResponseEntity.ok(msg)
+//    }
 }
+
+
+data class HelloMessage(
+    val id: String?,
+
+    @field: Size(max = 3)
+    val username: String,
+
+    val msg: String?
+)
