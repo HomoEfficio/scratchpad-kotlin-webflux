da3009247851c84f69c30c03f35da5d172602ac8
Homo Efficio <homo.efficio@gmail.com>
2021-04-27 09:57:38 +0900

    EnableReactiveMongoRepositories 추가

--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/Application.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/Application.kt
@@ -2,7 +2,9 @@ package io.homo_efficio.scratchpad.kotlin.webflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
+@EnableReactiveMongoRepositories
 @SpringBootApplication
 class Application
 
