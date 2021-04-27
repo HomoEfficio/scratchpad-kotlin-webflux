b55661ae5f44111df2cbf22efee95195958f083d
Homo Efficio <homo.efficio@gmail.com>
2021-04-27 11:22:54 +0900

    ReactiveMongoTransactionManager, TransactionalOperator 추가

--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/_config/MongoConfig.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/_config/MongoConfig.kt
@@ -1,8 +1,12 @@
 package io.homo_efficio.scratchpad.kotlin.webflux._config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.transaction.reactive.TransactionalOperator
 
 @Configuration
 @EnableReactiveMongoRepositories
@@ -10,5 +14,15 @@ class MongoConfig : AbstractReactiveMongoConfiguration() {
 
     override fun getDatabaseName(): String {
         return "my_reactive"
+    }
+
+    @Bean
+    fun transactionManager(reactiveMongoDatabaseFactory: ReactiveMongoDatabaseFactory): ReactiveMongoTransactionManager {
+        return ReactiveMongoTransactionManager(reactiveMongoDatabaseFactory)
+    }
+
+    @Bean
+    fun transactionalOperator(reactiveMongoTransactionManager: ReactiveMongoTransactionManager): TransactionalOperator {
+        return TransactionalOperator.create(reactiveMongoTransactionManager)
     }
 }
