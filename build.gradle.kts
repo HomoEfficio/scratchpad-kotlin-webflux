2a3287105b31d6a3167bb49aff3972e2fc940a3f
Homo Efficio <homo.efficio@gmail.com>
2021-04-27 10:14:32 +0900

    embedded mongodb를 testImplementation 에도 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.3")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 }
 
 tasks.withType<KotlinCompile> {
