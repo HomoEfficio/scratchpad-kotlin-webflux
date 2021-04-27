7942001bf55e41cdd7aa964476fa4a2648e81ed6
Homo Efficio <homo.efficio@gmail.com>
2021-04-27 09:58:07 +0900

    Hello 도메인 객체 import 수정

--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/domain/HelloRepository.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/domain/HelloRepository.kt
@@ -1,6 +1,5 @@
 package io.homo_efficio.scratchpad.kotlin.webflux.domain
 
-import io.homo_efficio.scratchpad.kotlin.webflux.domain.Hello
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
 interface HelloRepository : ReactiveMongoRepository<Hello, String>
--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/dto/HelloMessage.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/dto/HelloMessage.kt
@@ -1,5 +1,6 @@
 package io.homo_efficio.scratchpad.kotlin.webflux.dto
 
+import io.homo_efficio.scratchpad.kotlin.webflux.domain.Hello
 import javax.validation.constraints.Size
 
 data class HelloMessage(
@@ -9,4 +10,11 @@ data class HelloMessage(
     val username: String,
 
     val msg: String?
-)
+) {
+    companion object {
+        fun fromEntity(e: Hello): HelloMessage {
+            return HelloMessage(e.id, e.username, e.msg)
+        }
+    }
+}
+
