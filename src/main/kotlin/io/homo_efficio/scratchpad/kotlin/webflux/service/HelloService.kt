dbde35c8dd9990e4eafcfe3afcd5563349c791a4
Homo Efficio <homo.efficio@gmail.com>
2021-04-27 09:58:27 +0900

    findAll 추가

--- src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/service/HelloService.kt
+++ src/main/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/service/HelloService.kt
@@ -1,10 +1,12 @@
 package io.homo_efficio.scratchpad.kotlin.webflux.service
 
 import io.homo_efficio.scratchpad.kotlin.webflux.domain.Hello
-import io.homo_efficio.scratchpad.kotlin.webflux.dto.HelloMessage
 import io.homo_efficio.scratchpad.kotlin.webflux.domain.HelloRepository
+import io.homo_efficio.scratchpad.kotlin.webflux.dto.HelloMessage
+import kotlinx.coroutines.reactive.awaitFirstOrElse
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.stereotype.Service
+import java.util.Collections.unmodifiableList
 
 @Service
 class HelloService(
@@ -14,6 +16,14 @@ class HelloService(
     suspend fun save(message: HelloMessage): HelloMessage {
         val (id, username, msg) = message
         val dbHello = repo.save(Hello(id, username, msg)).awaitSingle()
-        return HelloMessage(dbHello.id, dbHello.username, dbHello.msg)
+        return HelloMessage.fromEntity(dbHello)
+    }
+
+    suspend fun findAll(): List<HelloMessage> {
+        return repo.findAll()
+            .map { hello -> HelloMessage.fromEntity(hello) }
+            .collectList()
+            .map { mutableList -> unmodifiableList(mutableList) as List<HelloMessage> }
+            .awaitFirstOrElse { emptyList() }
     }
 }
---
+++ src/test/kotlin/io/homo_efficio/scratchpad/kotlin/webflux/service/HelloServiceTest.kt
@@ -0,0 +1,42 @@
+package io.homo_efficio.scratchpad.kotlin.webflux.service
+
+import io.homo_efficio.scratchpad.kotlin.webflux.dto.HelloMessage
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@DataMongoTest
+@ExtendWith(SpringExtension::class)
+@Import(HelloService::class)
+internal class HelloServiceTest {
+
+    @Autowired private lateinit var svc: HelloService
+
+
+    @Test
+    fun `find all 2 messages`() {
+        runBlocking {  // runBlockingTest 사용 시 This job has not completed yet 발생 -> https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+            val msg1 = HelloMessage(
+                null,
+                "username-01",
+                "Message-01"
+            )
+            val msg2 = HelloMessage(
+                null,
+                "username-02",
+                "Message-02"
+            )
+            svc.save(msg1)
+            svc.save(msg2)
+
+            val actual: List<HelloMessage> = svc.findAll()
+
+            assertThat(actual.size).isEqualTo(2)
+        }
+    }
+}
